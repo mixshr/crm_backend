08b1f98e7693bdd1983530560d126bf6fa97c46d
mihailsarenko <mixail.sharenko@gmail.com>
2024-04-04 00:35:11 +0500

    first commit

--- src/main/java/org/mixshr/orderscontrollers/controller/OrderController.java
+++ src/main/java/org/mixshr/orderscontrollers/controller/OrderController.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import org.mixshr.orderscontrollers.dto.OrderDTO;
 import org.mixshr.orderscontrollers.entity.UserEntity;
 import org.mixshr.orderscontrollers.service.OrderService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -20,36 +21,47 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping("/all")
-    public List<OrderDTO> getAllOrders(
+    public ResponseEntity<List<OrderDTO>> getAllOrders(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "5") int size
     ) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return orderService.findAllByUser((UserEntity) authentication.getPrincipal(), page, size);
+        return ResponseEntity.ok().body(
+                orderService.findAllByUser((UserEntity) authentication.getPrincipal(), page, size
+                )
+        );
     }
 
     @GetMapping("/{id}")
-    public OrderDTO getOrderById(
+    public ResponseEntity<OrderDTO> getOrderById(
             @PathVariable UUID id
     ) {
-        return orderService.findById(id);
+        return ResponseEntity.ok().body(
+                orderService.findById(id)
+        );
     }
 
     @PostMapping("/save")
-    public OrderDTO saveOrder(
+    public ResponseEntity<OrderDTO> saveOrder(
             @RequestBody OrderDTO order
     ) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return orderService.save(order, (UserEntity) authentication.getPrincipal());
+        return ResponseEntity.ok().body(
+                orderService.save(order, (UserEntity) authentication.getPrincipal()
+                )
+        );
     }
 
     @PutMapping("/{id}")
-    public OrderDTO updateOrder(
+    public ResponseEntity<OrderDTO> updateOrder(
             @PathVariable UUID id,
             @RequestBody OrderDTO order
     ) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return orderService.update(id, order, (UserEntity) authentication.getPrincipal());
+        return ResponseEntity.ok().body(
+                orderService.update(id, order, (UserEntity) authentication.getPrincipal()
+                )
+        );
     }
 
     @DeleteMapping("/{id}")
